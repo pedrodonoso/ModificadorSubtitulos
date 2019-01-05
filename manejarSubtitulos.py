18cf96cacdf0c71429cc6edfdfaf015730f00cd5
Pedro Personal <pedro.donosoa@sansano.usm.cl>
2019-01-05 03:28:09 -0300

    Avance en manejo de texto

--- manejarSubtitulos.py
+++ manejarSubtitulos.py
@@ -1,13 +1,43 @@
-import re
-archivo = input("Escribe el nombre del archivo Subtitulo: ")
-file = open(archivo + ".txt",'r')
-for line in file:
-    print("match :")
+import re,time
 
-    m = re.match(r"(\w+) (\w+)",line)
-    m.group(0)
+def modifica(inicio,final,segundos):
+    segundo,milisegundo = inicio.strip().split(",")
+    h,m,s = segundo.split(":")
+    newsegundo = int(s) + segundos
+    lista = []
+    lista.append(h)
+    lista.append(m)
+    lista.append(str(newsegundo))
+    newinicio = ":".join(lista) + "," + milisegundo
     
-    print("linea :")
-    print(line)
+    segundo, milisegundo = final.strip().split(",")
+    h, m, s = segundo.split(":")
+    newsegundo = int(s) + segundos
+    lista = []
+    lista.append(h)
+    lista.append(m)
+    lista.append(str(newsegundo))
+    newfinal = ":".join(lista) + "," + milisegundo
+    return newinicio, newfinal
 
+
+file = open('sub.txt','r+')
+#ObjArchivo = open('/home/archivo.txt',mode='r', encoding='utf-8')
+lista = []
+c = 0
+while c < 6:
+    line = file.readline()
+    i = re.split(" (-->) ",line)
+    if i.__len__() > 2:
+        inicio = i[0]
+        final = i[2]
+        ni,nf = modifica(inicio,final,2)
+        nueva = ni + " --> " + nf + "\n"
+        lista.append(nueva)
+    else:
+        lista.append(line)
+    print(i)
+    c = c + 1
+
+print(lista)
 file.close()
