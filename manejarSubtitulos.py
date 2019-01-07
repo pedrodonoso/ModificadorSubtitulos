d50ccd4d4c3003f23cfbbd749a4f9986cae529a9
Pedro Personal <pedro.donosoa@sansano.usm.cl>
2019-01-07 02:25:39 -0300

    Casi listo, falta refinar.

--- manejarSubtitulos.py
+++ manejarSubtitulos.py
@@ -1,30 +1,30 @@
 import re,myTime
-def modifica(inicio,final,h,m,s):
-    newinicio = myTime.hacerTodo(inicio,h,m,s)
-    newfinal = myTime.hacerTodo(final,h,m,s)
-    print(newinicio, newfinal)
+
+def modifica(inicio,final,h,m,s,ms):
+    newinicio = myTime.hacerTodo(inicio,h,m,s,ms)
+    newfinal = myTime.hacerTodo(final,h,m,s,ms)
+
     return newinicio, newfinal
 
-
 file = open('sub.txt','r+')
+file2 = open('sub-remastered.txt','w')
 #ObjArchivo = open('/home/archivo.txt',mode='r', encoding='utf-8')
 lista = []
 c = 0
-while c < 6:
+while c < 60:
     line = file.readline()
     i = re.split(" (-->) ",line)
     if i.__len__() > 2:
         inicio = i[0]
         final = i[2]
-        ni,nf = modifica(inicio,final,0,0,2)
+        ni,nf = modifica(inicio,final,0,0,0,10)
         nueva = ni + " --> " + nf + "\n"
         lista.append(nueva)
+        file2.write(nueva)
     else:
         lista.append(line)
-    print(i)
+        file2.write(line)
     c = c + 1
 
 file.close()
-
-for i in lista:
-    print(i)
+file2.close()
